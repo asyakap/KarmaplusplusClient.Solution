02f35d6a8b817c3a350761d2a585f86ba0ef5912
Asia Kaplanyan <a.kaplanyan@gmail.com>
2023-05-12 10:56:59 -0700

    WIP - getting API call to work

--- Karmaplusplus/Controllers/CharityController.cs
+++ Karmaplusplus/Controllers/CharityController.cs
@@ -23,5 +23,27 @@ public class CharitiesController : Controller
         return View();
     }
 
+  [HttpPost, ActionName("Search")]
+  public async Task<IActionResult> Search(string name)
+  {
+    if(name == null)
+    {
+      return RedirectToAction("Index");
+    }
+    List<Charity> CharityList = new List<Charity> { };
+    using (var httpClient = new HttpClient())
+    {
+      using (var response = await httpClient.GetAsync($"https://partners.every.org/v0.2/browse/{name}?apiKey=pk_live_0010085fa96129b630dc18c80f8728f8"))
+      {
+        string apiResponse = await response.Content.ReadAsStringAsync();
+        JObject jsonResponse = JObject.Parse(apiResponse);
+        JArray charityArray = (JArray)jsonResponse["data"];
+        CharityList = charityArray.ToObject<List<Charity>>();
+      }
+    }
+    Charity charity = CharityList[0];
+    return View(charity);
+  }
+
 
 }
